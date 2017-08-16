de4e6fc66a23a43e0ca0ef438058418eaae8e639
Administrator <970171>
2017-08-16 18:18:42 +0800

    不想提交的

--- models/videoInfo.go
+++ models/videoInfo.go
@@ -286,7 +286,6 @@ func GetVideoByUid(yyuid string, limit int, page int) []VideoInfo{
 
 	defer func(){
 		beego.Error("出问题了")
-		return nil
 	}()
 	queryIntSlice := vidsIntList[(page-1)*limit:(page-1)*limit+limit]
 	fmt.Println(queryIntSlice)
